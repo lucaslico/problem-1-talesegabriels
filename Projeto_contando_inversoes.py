6cebec1683ab5dae02d5ff8bb40ecbd1abd05940
Lucas Lima <lucaslico3179@gmail.com>
2016-03-21 17:18:37 -0300

    Update Projeto_contando_inversoes.py
    Mudanças.

--- Projeto_contando_inversoes.py
+++ Projeto_contando_inversoes.py
@@ -1,24 +1,17 @@
-'''Primerio passo importar a lista'''
-import resource.py
-lista = [line.rstrip('\n') for line in open('C:\\Users\\Samsung\\Downloads\\IntegerArray.txt')]
+import resource
+lista = [line.rstrip('\n') for line in open('IntegerArray.txt')]
 for i in range(0,len(lista)):
     lista[i]=int(lista[i]) 
 
-'''Aplicando o algoritimo de inversão'''
-
 def merge_grande(array):
-    '''definindo um pivot'''
     meio=len(array)//2
-    
     if len(array)<2:
         return(array)
     esquerda=array[:meio]
-    print(esquerda)        #o que está acontecendo?
     direita=array[meio:]
-    print(direita)         #o que está acontecendo?
     resultado=merge_pequeno(merge_grande(esquerda),merge_grande(direita))    
-    print(resultado)      #o que está acontecendo?
-    return(resultado)    
+    return(resultado)
+
 
 inversoes = 0    
 
@@ -29,15 +22,12 @@ def merge_pequeno(arrayE,arrayD):
     global inversoes
     while i<len(arrayE) and j<len(arrayD):
         if arrayE[i]>arrayD[j]:
-            print(arrayE)               #o que está acontecendo?
             resposta.append(arrayD[j])
             inversoes = inversoes + (len(arrayE) - i)
             j=j+1
         else: 
             resposta.append(arrayE[i])
-            print(arrayE)              #o que está acontecendo?
             i=i+1
-    '''Aqui tudo que foi separado fica organizado novamente'''    
     resposta.extend(arrayE[i:]) 
     resposta.extend(arrayD[j:])
         
@@ -46,21 +36,6 @@ def merge_pequeno(arrayE,arrayD):
 def limit_memory(maxsize):
     soft, hard = resource.getrlimit(resource.RLIMIT_AS)
     resource.setrlimit(resource.RLIMIT_AS, (maxsize, hard))
-    
-    
-'''Fazendo um teste para verificar se está tudo funcionando '''
 
-listateste=[2,3,1,1]
-resposta_teste=merge_grande(listateste)
-print(resposta_teste)
-print(inversoes)
-
-'''Fazendo para a lista gigantesca'''
-
-resposta=merge_grande(lista)
-print(resposta) #vemos que não dá certo porque a memória vai pro espaço!!!
-
-'''tentando estratégias'''
-
-
-    
+merge_grande(lista)
+print inversoes
